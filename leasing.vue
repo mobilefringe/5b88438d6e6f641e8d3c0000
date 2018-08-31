372c7f83ee2104ab5fb0fabd085994503a380e36
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-31 16:54:52 -0400

    Lindsay Square 1.0  /leasing.vue

--- leasing.vue
+++ leasing.vue
@@ -83,9 +83,11 @@
             created() {
                 this.loadData().then(response => {
                     var temp_repo = this.findRepoByName('Leasing Banner');
-                    if(temp_repo != null) {
-                        this.pageBanner = temp_repo[0];
-                    } else {
+                    if(temp_repo !== null && temp_repo !== undefined) {
+                       temp_repo = temp_repo.images;
+                       this.pageBanner = temp_repo[0];
+                    }
+                    else {
                         this.pageBanner = {
                             "image_url": "//codecloud.cdn.speedyrails.net/sites/5b88438d6e6f641e8d3c0000/image/png/1531495616000/inside_banner.png"
                         }
