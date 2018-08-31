6d5d340ead40e1a2535e43b8a4f55dfdf135f28c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-31 15:55:24 -0400

    Lindsay Square 1.0  /welcome_msg.vue

--- welcome_msg.vue
+++ welcome_msg.vue
@@ -11,35 +11,10 @@
     define(["Vue", "vuex"], function(Vue, Vuex){
         return Vue.component("messages-component", {
             template: template, // the variable template will be injected
-            data: function() {
-                return {
-                    currentMessage: null
-                }
-            },
-            created(){
-                this.loadData().then(response => {
-                    if(response[0].data.messages.welcome[0] != undefined){
-                        this.currentMessage = response[0].data.messages.welcome[0].messages[0];
-                    } else {
-                        console.log("No message at this time.");       
-                    }
-                });
-            },
             computed: {
                 ...Vuex.mapGetters([
                     'property'
                 ])
-            },
-            methods: {
-                loadData: async function() {
-                    this.property.mm_host = this.property.mm_host.replace("http:", "");
-                    try {
-                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/api/v3/gerrardsquare/messages.json"})]);
-                        return results;
-                    } catch(e) {
-                        console.log("Error loading data: " + e.message);    
-                    }
-                }
             }
         })
     });
