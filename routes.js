1e6ada1413449aeab203f9cd79dd8f90b7f6b408
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-07 11:54:08 -0400

    Lindsay Square 1.0  /routes.js

--- routes.js
+++ routes.js
@@ -219,7 +219,7 @@ define([], function () {
                     meta: {
                         pageName: 'Search Results',
                     },
-                    name: 'searchResutls'
+                    name: 'search-results'
                 }
             ]
         },
