d405196d53619ec193a39208182426b8728e5d1f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-11 17:20:57 -0400

    Lindsay Square 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -92,7 +92,7 @@
                             }
                         } else {
                             if  (_.includes(this.currentPromo.promo_image_url_abs, 'missing')) {
-                                this.currentPromo.image_url = "//codecloud.cdn.speedyrails.net/sites/5b88438d6e6f641e8d3c0000/image/png/1531496516000/promo placeholder.png";    
+                                this.currentPromo.image_url = site.default_logo_url;//"//codecloud.cdn.speedyrails.net/sites/5b88438d6e6f641e8d3c0000/image/png/1531496516000/promo placeholder.png";    
                             }
                         }
                     }
