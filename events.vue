b976b290f97e23138ca50bbc911d4afa1e709255
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-11 17:21:48 -0400

    Lindsay Square 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -116,7 +116,7 @@
                             }
                             
                             if (_.includes(value.image_url, 'missing')) {
-                                value.image_url = "//codecloud.cdn.speedyrails.net/sites/5b88438d6e6f641e8d3c0000/image/png/1531496511000/event placeholder.png";
+                                value.image_url = site.default_logo_url;"//codecloud.cdn.speedyrails.net/sites/5b88438d6e6f641e8d3c0000/image/png/1531496511000/event placeholder.png";
                             }
                             
                             value.description_short = _.truncate(value.description, { 'length': 100, 'separator': ' ' });
