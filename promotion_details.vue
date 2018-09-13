f8fe06bd2d277a975a7347ee87ff2363cac47a53
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-13 11:32:26 -0400

    Lindsay Square 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -79,17 +79,17 @@
 						this.$router.replace({ path: '/promotions' });
 					}
 					else {
-					    if(this.currentPromo != null) {
-                        if (this.currentPromo.promotionable_type === "Store"){
-                            if  (_.includes(this.currentPromo.promo_image_url_abs, 'missing')) {
-                                this.currentPromo.image_url = this.currentPromo.store.store_front_url_abs; 
-                            }
-                        } else {
-                            if  (_.includes(this.currentPromo.promo_image_url_abs, 'missing')) {
-                                this.currentPromo.image_url = site.default_logo_url;//"//codecloud.cdn.speedyrails.net/sites/5b88438d6e6f641e8d3c0000/image/png/1531496516000/promo placeholder.png";    
-                            }
-                        }
-                    }
+					   // if(this.currentPromo != null) {
+        //                     if (this.currentPromo.promotionable_type === "Store"){
+        //                         if  (_.includes(this.currentPromo.promo_image_url_abs, 'missing')) {
+        //                             this.currentPromo.image_url = this.currentPromo.store.store_front_url_abs; 
+        //                         }
+        //                     } else {
+        //                         if  (_.includes(this.currentPromo.promo_image_url_abs, 'missing')) {
+        //                             this.currentPromo.image_url = site.default_logo_url;//"//codecloud.cdn.speedyrails.net/sites/5b88438d6e6f641e8d3c0000/image/png/1531496516000/promo placeholder.png";    
+        //                         }
+        //                     }
+        //                 }
 					}
 					this.dataLoaded = true;
 				}, error => {
