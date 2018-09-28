7932ee3764f67c36fcf58ae97c4449b3d4cfa120
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-28 14:14:12 -0400

    Lindsay Square 1.0  /leasing_temporary.vue

--- leasing_temporary.vue
+++ leasing_temporary.vue
@@ -161,7 +161,7 @@
                             send_data.url = "https://www.mallmaverick.com/send_contact_email";
                             var temp_formdata = {}; //JSON.stringify(this.serializeObject(this.form_data));
                             temp_formdata.send_to = "mcgoverna@davpart.com";
-                            temp_formdata.subject = "Gerrard Square Permanent Leasing Form"; 
+                            temp_formdata.subject = "Lindsay Square Permanent Leasing Form"; 
                             temp_formdata.body = {};
                             temp_formdata.body["Legal Name of Organization"] =  this.form_data.legalNameTemp;
                             temp_formdata.body["Contact First Name"] =   this.form_data.firstNameTemp, 
