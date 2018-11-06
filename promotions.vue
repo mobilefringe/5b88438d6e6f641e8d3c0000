78b5f00237e3ca7cee091d72eca9598e0c9df9e9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-06 11:50:58 -0500

    Lindsay Square 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -85,7 +85,7 @@
             created (){
                 this.loadData().then(response => {
                     var temp_repo = this.findRepoByName('Promotions Banner');
-                    if(temp_repo !== null && temp_repo !== undefined) {
+                    if(temp_repo && temp_repo.images) {
                        temp_repo = temp_repo.images;
                        this.pageBanner = temp_repo[0];
                     }
