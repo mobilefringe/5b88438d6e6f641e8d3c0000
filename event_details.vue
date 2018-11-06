29cda78671bbe50d3958bc5b4259d9b7119ad2d8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-06 11:57:57 -0500

    Lindsay Square 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -75,7 +75,14 @@
                         "image_url": "//codecloud.cdn.speedyrails.net/sites/5b88438d6e6f641e8d3c0000/image/png/1531495616000/inside_banner.png"
                     }
                 }
-                    
+                var temp_repo1 = this.findRepoByName('Events Side Banner');
+                if(temp_repo1 && temp_repo1.images) {
+                    this.sideBanner = temp_repo1.images[0];
+                } else {
+                    this.sideBanner = {
+                        "image_url": ""
+                    }
+                }      
 				this.$store.dispatch("getData", "events").then(response => {
 					this.currentEvent = this.findEventBySlug(this.id);
 					if (this.currentEvent === null || this.currentEvent === undefined) {
