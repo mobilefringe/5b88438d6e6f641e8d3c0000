6a00fe74560d90843b1030fecc5d8dad7a9172a9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-06 11:58:23 -0500

    Lindsay Square 1.0  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -74,7 +74,14 @@
                             "image_url": "//codecloud.cdn.speedyrails.net/sites/5b88438d6e6f641e8d3c0000/image/png/1531495616000/inside_banner.png"
                         }
                     }
-                    
+                    var temp_repo1 = this.findRepoByName('Jobs Side Banner');
+                    if(temp_repo1  && temp_repo1.images) {
+                        this.sideBanner = temp_repo1.images[0];
+                    } else {
+                        this.sideBanner = {
+                            "image_url": ""
+                        }
+                    }
 				this.$store.dispatch("getData", "jobs").then(response => {
 					this.currentJob = this.findJobBySlug(this.id);
 					if (this.currentJob === null || this.currentJob === undefined) {
